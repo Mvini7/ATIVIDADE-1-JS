318127771258a632728bf25fe4deb092f79fcaa3
@viniciuswqi <158781792+Mvini7@users.noreply.github.com>
2024-09-16 17:09:56 -0300

    Add files via upload

---
+++ AULA 2 (LOGICA DE PROGRAMAÇÃO)/atv01.py
@@ -0,0 +1,7 @@
+palavra1 = input("Digite a primeira palavra: ")
+palavra2 = input("Digite a segunda palavra: ")
+
+if palavra1 == palavra2:
+    print("A primeira idade é igual que a segunda.")
+else:
+    print("As duas palavras não são iguais.")
